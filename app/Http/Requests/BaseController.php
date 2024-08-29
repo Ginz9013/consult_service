434149850b9aae0140c6799fe5c0d7a31a79c75b
Alan Ho <alan.ho@contin-global.com>
2024-08-29 14:16:39 +0800

    feat: 建立 BseController 修正驗證失敗回傳 json

---
+++ app/Http/Requests/BaseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseController extends FormRequest
+{
+    public function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation failed',
+            'errors' => $errors
+        ], 422));
+    }
+}
