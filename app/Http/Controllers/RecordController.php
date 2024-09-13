70a3e377ae73eef69a9105e4170249fef463d129
Alan Ho <alan.ho@contin-global.com>
2024-09-13 15:38:39 +0800

    fix: 修正 create Daily

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -3,44 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Records\CreateDailyRecordRequest;
-use App\Models\User;
 use App\Models\Daily;
-use App\Models\Dietary;
 
 class RecordController extends Controller
 {
     // Create Daily Record
     public function createDailyRecord(CreateDailyRecordRequest $request) {
 
-        // User ID
+        
+        $validatedData = $request->validated();
+        
+        // Create Daily
+        $newDaily = new Daily($validatedData);
+
+        // Save Daily to User
         $user = auth()->user();
-
-        $data = $request->all();
-        $daily = data_get($data, 'data.daily');
-
-        // Save Daily
-        $newDaily = new Daily($daily);
         $user->dailies()->save($newDaily);
-
-        // Save Dietary
-        $dietaries = data_get($data, 'data.dietaries');
-
-        if($dietaries) {
-            foreach($dietaries as $dietary) {
-
-                if(data_get($dietary, 'images')) unset($dietary['images']);
-                
-                $newDietary = new Dietary($dietary);
-                $newDaily->dietaries()->save($newDietary);
-            }
-        };
 
         return response()->json([
             'status' => 200,
             'message' => 'Successfully created.',
             'data' => [
-                'daily' => $newDaily,
-                'dietaries' => $newDaily->dietaries()->get()
+                'daily' => $newDaily
             ]
         ]);
     }
--- app/Http/Requests/Records/CreateDailyRecordRequest.php
+++ app/Http/Requests/Records/CreateDailyRecordRequest.php
@@ -14,30 +14,18 @@ class CreateDailyRecordRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            // ====== Daily ======
-            'data.daily' => 'required|array',
-            'data.daily.date' => 'required|date_format:Y-m-d',
-            'data.daily.weight' => 'nullable|numeric|min:1',
-            'data.daily.body_fat' => 'nullable|numeric|max:1',
-            'data.daily.note' => 'nullable|string',
-            'data.daily.water_morning' => 'nullable|numeric|min:0',
-            'data.daily.water_afternoon' => 'nullable|numeric|min:0',
-            'data.daily.water_evening' => 'nullable|numeric|min:0',
-            'data.daily.water_another' => 'nullable|numeric|min:0',
-            'data.daily.coffee' => 'nullable|numeric|min:0',
-            'data.daily.tea' => 'nullable|numeric|min:0',
-            
-            // ====== Dietary ======
-            'data.dietaries' => 'nullable|array',
-            'data.dietaries.*.time' => 'required|date_format:H:i:s',
-            'data.dietaries.*.staple' => 'nullable|numeric|min:0',
-            'data.dietaries.*.meat' => 'nullable|numeric|min:0',
-            'data.dietaries.*.fruit' => 'nullable|numeric|min:0',
-            'data.dietaries.*.vegetable' => 'nullable|numeric|min:0',
-            'data.dietaries.*.fat' => 'nullable|numeric|min:0',
-            'data.dietaries.*.description' => 'nullable|string',
-            'data.dietaries.*.images' => 'nullable|array|max:3',
-            'data.dietaries.*.images.*' => 'string',
+            'date' => 'bail|required|date_format:Y-m-d',
+            'weight' => 'bail|nullable|numeric|min:1',
+            'body_fat' => 'bail|nullable|numeric|max:1',
+            'water_morning' => 'bail|nullable|numeric|min:0',
+            'water_afternoon' => 'bail|nullable|numeric|min:0',
+            'water_evening' => 'bail|nullable|numeric|min:0',
+            'water_another' => 'bail|nullable|numeric|min:0',
+            'coffee' => 'bail|nullable|numeric|min:0',
+            'tea' => 'bail|nullable|numeric|min:0',
+            'sport' => 'bail|nullable|string',
+            'defecation' => 'bail|nullable|string',
+            'note' => 'bail|nullable|string'
         ];
     }
 }
